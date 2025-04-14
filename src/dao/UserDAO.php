9305bd422dfc542e21e12b34e110925288342a56
Thiago777P <thiagogm333@gmail.com>
2025-04-14 15:39:08 -0300

    atualizações

--- src/dao/UserDAO.php
+++ src/dao/UserDAO.php
@@ -12,19 +12,20 @@ class UserDAO {
     }
 
     public function create($nome, $email, $senha) {
-        $query = "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
-        return $this->db->executar($query, [
+        $query = "INSERT INTO usuarios (nome, email, password) VALUES (:nome, :email, :senha)";
+        $result = $this->db->executar($query, [
             ':nome' => $nome,
             ':email' => $email,
             ':senha' => password_hash($senha, PASSWORD_DEFAULT)
         ]);
+        
+        return $result !== false;
     }
 
     public function findByEmail($email) {
-        $query = "SELECT * FROM usuarios WHERE email = :email";
-        $result = $this->db->executar($query, [
-            ':email' => $email
-        ]);
-        return $result ? $result[0] : null;
+        $query = "SELECT * FROM usuarios WHERE email = :email LIMIT 1";
+        $result = $this->db->executar($query, [':email' => $email]);
+        
+        return !empty($result) ? $result[0] : null;
     }
 }
